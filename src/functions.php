d0f77025a6aceb3646358e3679abdd4883390c2f
Administrator <howmao@qq.com>
2024-05-08 09:30:43 +0800

    add to_snake_plural

--- src/functions.php
+++ src/functions.php
@@ -1,5 +1,14 @@
 <?php
 
+use Illuminate\Support\Str;
+
+/**
+ *
+ * @deprecated 该方法废弃，请使用 to_standard_object_name
+ * 获取标准对象名称
+ * @param string $objectName
+ * @return string
+ */
 function get_standard_object_name(string $objectName)
 {
     $words = explode("_", \Illuminate\Support\Str::snake($objectName));
@@ -7,4 +16,30 @@ function get_standard_object_name(string $objectName)
         $word = \Illuminate\Support\Str::ucfirst(\Illuminate\Support\Str::singular($word));
     }
     return implode("", $words);
+}
+
+/**
+ *
+ * 将对象名称转换为 单数
+ * @param string $objectName
+ * @return string
+ */
+function to_standard_object_name(string $objectName)
+{
+    $words = explode("_", \Illuminate\Support\Str::snake($objectName));
+    foreach ($words as &$word) {
+        $word = \Illuminate\Support\Str::ucfirst(\Illuminate\Support\Str::singular($word));
+    }
+    return implode("", $words);
+}
+
+/**
+ *
+ * 目标字符串
+ * @param string $targetString
+ * @return string
+ */
+function to_snake_plural(string $targetString)
+{
+    return Str::plural(Str::snake($targetString, "_"));
 }
